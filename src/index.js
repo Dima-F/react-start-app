eb4e097341626a4a90e6379ce0cf0e4d16329716
Dima <dafw@live.ru>
2018-04-13 18:01:27 +0300

    redux start

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+// import {Router, Route,} from 'react-router';
+// 
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { createStore } from 'redux';
+
+function playList(state = [], action){
+    if(action.type==='ADD_TRACK'){
+        return [
+            ...state,
+            action.payload
+        ]
+    }
+    return state;
+}
+
+const store = createStore(playList);
+
+store.subscribe(()=>{
+    console.log('subscribe',store.getState());
+});
+
+store.dispatch({type:'ADD_TRACK', payload:'Smels like spirit'});
+store.dispatch({type:'ADD_TRACK', payload:'Enter sendman'});
